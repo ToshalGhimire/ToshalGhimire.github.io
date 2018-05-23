0bb8bee546a2539fa89af6125b0f3bf4ccfd8aa6
Toshal Ghimire <togh7596@colorado.edu>
2018-05-23 14:52:24 -0600

    Updated HTML with Div tags

--- script/Script.js
+++ script/Script.js
@@ -1,10 +1,16 @@
+var elem = document.getElementById('Container');
+elem.style.width =  70+ "%";
+
 function toggleMenu(){
 	var x = document.getElementById("navbarList");
+	var menu = document.getElementById("handle");
 	
 	if(x.className == "navbar"){
+		
 		x.className += "responsive";
 	}else{
 		x.className = "navbar";
 	}
 	
 }
+
